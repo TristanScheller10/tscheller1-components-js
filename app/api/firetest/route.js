d20ae48a84e484a5f7a60015598716fca1cddd79
tristan <tristan.i.scheller@gmail.com>
2023-11-16 23:15:37 -0700

    ninth-commit

--- app/api/firetest/route.js
+++ app/api/firetest/route.js
@@ -2,7 +2,7 @@ import {db} from '@/lib/firebase/firebase-config'
 async function GET (req, {params}) {
     const dbRef = db.ref('/foods')
     const dataSnapshot = await dbRef.once('value')
-    const records = await dataSnapshot.val()
+    const foods = await dataSnapshot.val()
 
     return Response.json(foods)
 }
