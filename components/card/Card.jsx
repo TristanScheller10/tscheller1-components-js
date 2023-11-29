78a0e83fb40dc0b2520c8c9d9ebfd06adf0afff5
tristan <tristan.i.scheller@gmail.com>
2023-11-28 22:31:14 -0700

    third-assign-6th-commit

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -10,7 +10,7 @@ function Card ({id, food_name, food_category, food_price, food_calories }){
              <p>Price: {food_price}</p>
              <p>Calories: {food_calories}</p>
             <Link className='text-blue-600 font-semibold' href={`/food/${id}`}> view more about: {food_name}</Link>
-            <div className='flex py-2'><a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Purchase </a></div>
+            <div className='flex py-2'><a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Purchase</a></div>
            </aside>
     )
   }
